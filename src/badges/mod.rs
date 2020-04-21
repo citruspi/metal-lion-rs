b00ba8e230691d7bb4105efe80b9f98ac40088e1
Mihir Singh <git.service@mihirsingh.com>
2020-04-21 06:19:00 +0000

    Added cloneable badges::Factory and FactoryOptions types and implemented Factory::new

--- src/badges/mod.rs
+++ src/badges/mod.rs
@@ -3,3 +3,21 @@ pub struct SvgBadgeInput {
     pub title: String,
     pub text: String,
 }
+
+#[derive(Clone)]
+pub struct FactoryOptions {
+    pub render_dataset: minutiae::DataSet,
+}
+
+#[derive(Clone)]
+pub struct Factory {
+    opts: FactoryOptions,
+}
+
+impl Factory {
+    pub fn new(opts: FactoryOptions) -> Factory {
+        info!("building factory");
+
+        Factory { opts }
+    }
+}
