e7fb36f093266515847aff1d907256d6010129a5
Mihir Singh <git.service@mihirsingh.com>
2020-04-21 08:07:48 +0000

    Added support for determining if a font is supported and retrieving defaults

--- src/badges/mod.rs
+++ src/badges/mod.rs
@@ -23,6 +23,19 @@ impl Factory {
         Factory { opts }
     }
 
+    pub fn default_font_face(&self) -> minutiae::FontFace {
+        self.opts.render_dataset.config.font.faces[0].clone()
+    }
+
+    pub fn default_font_size(&self) -> minutiae::FontSize {
+        self.opts.render_dataset.config.font.sizes[0].clone()
+    }
+
+    pub fn supports_font(&self, face: minutiae::FontFace, size: minutiae::FontSize) -> bool {
+        self.opts.render_dataset.config.font.faces.contains(&face)
+            && self.opts.render_dataset.config.font.sizes.contains(&size)
+    }
+
     pub fn render_svg(&self, input: SvgBadgeInput) -> String {
         let bbox = self.opts.render_dataset.bounding_box(
             &format!("{} - {}", input.title, input.text),
